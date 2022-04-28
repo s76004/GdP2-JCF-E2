d93356152f4be501af277c1e670a53041d570ea9
heshsum <mail@heshsum.de>
2022-04-28 22:14:40 +0200

    Added first helper methods: add(), resize(), getSize()

--- src/MeineArrayList.java
+++ src/MeineArrayList.java
@@ -1,26 +1,54 @@
 /**
- * a very simplified version of java.util.ArrayList 
+ * a very simplified version of java.util.ArrayList
  *
  * @param <E>
  */
 
 public class MeineArrayList<E> {
-	private E[] list;
-	private int size;
-	private int capacity;
-	private int increment;
+    private E[] list;
+    private int size;
+    private int capacity;
+    private int increment;
 
-	/**
-	 * constructs a MeineArrayList-Object with initial capacity c
-	 * and increment inc
-	 * @param c the initial capacity
-	 * @param inc the number of positions that will be added
-	 * when the list is made bigger
-	 */
-	public MeineArrayList(int c, int inc) {
-		capacity = c;
-		list = (E[]) new Object[c];
-		size = 0;
-		increment = inc;
-	}
+    /**
+     * constructs a MeineArrayList-Object with initial capacity c
+     * and increment inc
+     *
+     * @param c   the initial capacity
+     * @param inc the number of positions that will be added
+     *            when the list is made bigger
+     */
+    public MeineArrayList(int c, int inc) {
+        capacity = c;
+        list = (E[]) new Object[c];
+        size = 0;
+        increment = inc;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(E e) {
+        if(this.size >= this.capacity) {
+            resize();
+        }
+        list[size] = e;
+    }
+
+    public void remove(E e) {
+    }
+
+    public Object get(int i) {
+        return list[i];
+    }
+
+    private void resize() {
+        E[] newList = (E[]) new Object[this.capacity + this.increment];
+        for(int i = 0; i < this.size; i++) {
+            newList[i] = (E) this.get(i);
+        }
+        this.list = newList;
+        this.capacity = newList.length;
+    }
 }
