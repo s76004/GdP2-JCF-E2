00f2ec3788248cea62b07055af36c52e3d6310c9
heshsum <mail@heshsum.de>
2022-04-29 20:58:24 +0200

    JavaDoc and formatting

--- src/MeineArrayListTest.java
+++ src/MeineArrayListTest.java
@@ -1,23 +1,35 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
-
+/**
+ * Tester-Klasse für MeineArrayList.
+ *
+ * @version 1.0
+ */
 public class MeineArrayListTest {
-
+    /**
+     * Eine Beispiel-MeineArrayList.
+     */
     MeineArrayList testList = new MeineArrayList(13, 2);
 
+    /**
+     * Test-Methode für die Methode egtSize().
+     */
     @Test
-    public void getsizeMyArrayList() {
+    public void getSizeTest() {
         MeineArrayList ml = new MeineArrayList(10, 3);
-        assertEquals(0, ml.getSize(),"Size wrong");
-    }
-    @Test
-    public void getcapacityMyArrayList() {
-        MeineArrayList ml = new MeineArrayList(10, 3);
-        assertEquals(10, ml.getCapacity(),"capacity wrong");
+        assertEquals(0, ml.getSize(), "Size wrong");
     }
 
-
+    /**
+     * Test-Methode für die Methode getCapacity().
+     */
+    @Test
+    public void getCapacityTest() {
+        MeineArrayList ml = new MeineArrayList(10, 3);
+        assertEquals(10, ml.getCapacity(), "capacity wrong");
+    }
 
     /**
      * Helper Methode um Logging zu vereinfachen.
@@ -33,8 +45,11 @@ public class MeineArrayListTest {
         System.out.println(logString);
     }
 
+    /**
+     * Test-Methode für die Methode log().
+     */
     @Test
-    public void logtest() {
+    public void logTest() {
         testList.add("a");
         testList.add("b");
         testList.add("c");
@@ -44,9 +59,12 @@ public class MeineArrayListTest {
 
     }
 
+    /**
+     * Test-Methode für die Methode add().
+     */
     @Test
-    public void testadd() {
-        System.out.println("\ntestadd()");
+    public void testAdd() {
+        log("\ntestadd()");
         MeineArrayList addTestList = new MeineArrayList<>(10, 2);
         addTestList.add("Maria");
         addTestList.add("Joseph");
@@ -59,9 +77,12 @@ public class MeineArrayListTest {
         log("Array-Groesse:\t\t\t\tSoll = 13\tIst = " + addTestList.getCapacity());
     }
 
+    /**
+     * Test-Methode für die Methode add() mit Vergrößerung der MeineArrayList.
+     */
     @Test
     public void testAddResize() {
-        System.out.println("\ntestadd()");
+        log("\ntestadd()");
         MeineArrayList addTestList = new MeineArrayList<>(2, 2);
         addTestList.add("Maria");
         addTestList.add("Joseph");
@@ -75,65 +96,77 @@ public class MeineArrayListTest {
         log("Array-Groesse:\t\t\t\tSoll = 4\tIst = " + addTestList.getCapacity());
     }
 
+    /**
+     * Test-Methode für die Methode remove().
+     */
     @Test
     public void testRemove() {
         log("\ntestRemoveEntry()");
         MeineArrayList removeTestList = new MeineArrayList<>(10, 2);
-        String Maria = "Maria";
-        String Joseph = "Joseph";
-        String Jesus = "Jesus";
-        String Eva = "Eva";
-        String Judas = "Judas";
+        String maria = "Maria";
+        String joseph = "Joseph";
+        String jesus = "Jesus";
+        String eva = "Eva";
+        String judas = "Judas";
 
-        removeTestList.add(Maria);
-        removeTestList.add(Joseph);
-        removeTestList.add(Jesus);
-        removeTestList.add(Eva);
-        removeTestList.add(Judas);
+        removeTestList.add(maria);
+        removeTestList.add(joseph);
+        removeTestList.add(jesus);
+        removeTestList.add(eva);
+        removeTestList.add(judas);
 
         log(removeTestList);
 
         log("Anzahl der Array-Elemente vor dem Loeschen: " + removeTestList.getSize());
         log("Inhalt des Arrays vor dem Loeschen: " + removeTestList);
         log("Geloescht wird Element 'Maria'.");
-        removeTestList.remove(Maria);
+        removeTestList.remove(maria);
         assertEquals(4, removeTestList.getSize());
-        System.out.println("Anzahl der Array-Elemente nach dem Loeschen: " + removeTestList.getSize());
-        System.out.println("Inhalt des Arrays nach dem Loeschen: " + removeTestList);
+        log("Anzahl der Array-Elemente nach dem Loeschen: " + removeTestList.getSize());
+        log("Inhalt des Arrays nach dem Loeschen: " + removeTestList);
     }
 
+    /**
+     * Test-Methode für die Methode removeAll().
+     */
     @Test
     public void removeAll() {
         System.out.println("\ntestRemoveAll()");
         testList.add("Maria");
         testList.add("Joseph");
         testList.add("Jesus");
-        System.out.println("Inhalt des Arrays vor dem Loeschen: " + testList.toString());
+        log("Inhalt des Arrays vor dem Loeschen: " + testList.toString());
         testList.removeAll();
         assertEquals(0, testList.getSize());
-        System.out.println("Inhalt des Arrays nach dem Loeschen: " + testList.toString());
+        log("Inhalt des Arrays nach dem Loeschen: " + testList.toString());
     }
 
+    /**
+     * Test-Methode für die Methode get().
+     */
     @Test
-    public void testget() {
-        System.out.println("\ntestget()");
+    public void testGet() {
+        System.out.println("\ntestGet()");
         testList.add("Maria");
         testList.add("Joseph");
         testList.add("Jesus");
         assertEquals("Jesus", testList.get(2));
-        System.out.println("Inhalt des Arrays: " + testList.toString());
-        System.out.println("Abgefragt wird Element 2.\t\tSoll-Wert = \"Jesus\" Ist-Wert \"" + testList.get(2) + "\"");
+        log("Inhalt des Arrays: " + testList);
+        log("Abgefragt wird Element 2.\t\tSoll-Wert = \"Jesus\" Ist-Wert \"" + testList.get(2) + "\"");
     }
 
+    /**
+     * Test-Methode für die Methode getSize().
+     */
     @Test
     public void testGetSize() {
-        System.out.println("\ntestGetSize()");
+        log("\ntestGetSize()");
         testList.add("Maria");
         testList.add("Joseph");
         testList.add("Jesus");
         assertEquals(3, testList.getSize());
-        System.out.println("Inhalt des Arrays: " + testList.toString());
-        System.out.println("Anzahl der Array-Elemente:\t\tSoll = 3\tIst = " + testList.getSize());
+        log("Inhalt des Arrays: " + testList.toString());
+        log("Anzahl der Array-Elemente:\t\tSoll = 3\tIst = " + testList.getSize());
     }
 }
 
